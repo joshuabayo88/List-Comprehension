4b5e0914529be616ba80da5f931b8389eeea0a88
joshuabayo88 <joshuabayo88@gmail.com>
2022-01-26 15:50:33 +0100

    Conversion into Dataframe

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
-# my_numbers = [1, 2, 3, 4]
-# new_numbers = ([n + 1 for n in my_numbers])
-# print(new_numbers)
+import pandas as pd
+
+my_numbers = [1, 2, 3, 4]
+new_numbers = [n + 1 for n in my_numbers]
+print(new_numbers)
 
 name = "Angela"
 letters_list = [letter for letter in name if letter != "a"]
@@ -21,5 +23,18 @@ cap_name = [name.upper() for name in name_list]
 print(cap_name)
 
 # Dictionary Comprehension
-students_score = {student:random.randint(1, 100) for student in name_list}
+students_score = {student:random.randint(1, 100) for student in name_list if random.randint(1, 100) > 50}
 print(students_score)
+
+# Iterate over Pandas Dataframe.
+
+student_dict = {
+    "student": ["Bayo", "Joshua", "Beth", "Alex", "Jossy"],
+    "scores": [56, 76, 96, 84, 61]
+}
+student_data_frame = pd.DataFrame(student_dict)
+print(student_data_frame)
+for(index, row) in student_data_frame.iterrows():
+    if row.student == "Bayo":
+        print(row.student)
+        print(row.scores)
